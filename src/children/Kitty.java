07a0763ca64ffc626bfce9dbdb05cbd1e54bb6ba
jiabhe <jiabhe@D-SHC-00802350.corp.ebay.com>
2016-07-29 10:55:48 +0800

    kitty

--- src/children/Kitty.java
+++ src/children/Kitty.java
@@ -1,9 +1,13 @@
 package children;
 
+import java.util.logging.Logger;
+
 import parent.Cat;
 
 public class Kitty extends Cat{
+	static String className = Kitty.class.getName();
+	static Logger logger = Logger.getLogger(className);
 	public void mew(){
-		System.out.println("mew mew woo");
+		logger.fine("mew mew mew");
 	}
 }
